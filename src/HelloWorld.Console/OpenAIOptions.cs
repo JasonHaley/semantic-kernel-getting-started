a8bed316e4bf86bd180960d2fdc1f59cf6753351
JasonHaley <jason@jasonhaley.com>
2024-03-30 16:26:09 -0400

    Inititial code checkin

---
+++ src/HelloWorld.Console/OpenAIOptions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HelloWorld.Console;
+internal class OpenAIOptions
+{
+    public const string OpenAI = "OpenAI";
+
+    public string Source { get; set; } = string.Empty;
+    public string ChatModelId { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string ChatDeploymentName { get; set; } = string.Empty;
+    public string Endpoint { get; set; } = string.Empty;
+}
---
+++ src/HelloWorld.Console/Program.cs
@@ -0,0 +1,108 @@
+﻿using HelloWorld.Console;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Azure.AI.OpenAI;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MainAsync(args).Wait();
+    }
+    static async Task MainAsync(string[] args)
+    {
+        var config = Configuration.ConfigureAppSettings();
+
+        // Get Settings (all this is just so I don't have hard coded config settings here)
+        var openAiSettings = new OpenAIOptions();
+        config.GetSection(OpenAIOptions.OpenAI).Bind(openAiSettings);
+
+        // Uncomment to add OpenTelemetry as a logging provider
+        //using var meterProvider = Sdk.CreateMeterProviderBuilder()
+        //    .AddMeter("Microsoft.SemanticKernel*")
+        //    .AddConsoleExporter()
+        //    .Build();
+
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Information);
+
+            // Uncomment to add OpenTelemetry as a logging provider
+            //builder.AddOpenTelemetry(options =>
+            //{
+            //    options.AddConsoleExporter();
+            //    options.IncludeFormattedMessage = true;
+            //});
+
+            builder.AddConfiguration(config);
+            builder.AddConsole();
+        });
+
+        // Configure Semantic Kernel
+        var builder = Kernel.CreateBuilder();
+                
+        builder.Services.AddSingleton(loggerFactory);
+        builder.Services.AddChatCompletionService(openAiSettings);
+
+        Kernel kernel = builder.Build();
+
+        // --------------------------------------------------------------------------------------
+        // Exercise from Virtual Boston Azure for creating a prompt
+        // --------------------------------------------------------------------------------------
+
+        // output today's date just for fun
+        
+        WriteLine($"\n----------------- DEBUG INFO -----------------");
+        var today = DateTime.Now.ToString("MMMM dd");
+        WriteLine($"Today is {today}");
+        WriteLine("----------------------------------------------");
+
+        // TODO: CHALLENGE 1: does the AI respond accurately to this prompt? How to fix?
+        var prompt = $"Tell me an interesting fact from world about an event " +
+                    $"that took place on {today}. " +
+                    $"Be sure to mention the date in history for context.";
+
+        WriteLine($"\nPROMPT: \n\n{prompt}");
+        WriteLine("----------------------------------------------");
+                                
+        IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+        ChatHistory chatMessages = [];
+        chatMessages.AddUserMessage(prompt);
+
+        OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+        {
+            Temperature = 0.7f,
+            MaxTokens = 250
+        };
+
+        var result = await chatCompletionService.GetChatMessageContentsAsync(chatMessages, openAIPromptExecutionSettings, kernel);
+
+        // Write out the result
+        foreach (var content in result)
+        {
+            WriteLine($"\nRESPONSE:\n{content}");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+    }
+
+    static void WriteLine(string message)
+    {
+        var currentColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+        
+        Console.WriteLine(message);
+        
+        Console.ForegroundColor = currentColor;
+    }
+}
