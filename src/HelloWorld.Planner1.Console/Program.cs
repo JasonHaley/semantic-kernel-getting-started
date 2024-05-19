66453e2374556f74965e0e3e48ade2137fee494f
JasonHaley <jason@jasonhaley.com>
2024-05-19 14:45:08 -0400

    refactoring to clean up syntax

--- src/HelloWorld.Planner1.Console/Program.cs
+++ src/HelloWorld.Planner1.Console/Program.cs
@@ -7,6 +7,7 @@ using OpenTelemetry.Logs;
 using HelloWorld.Plugin.Console.Plugins;
 using HelloWorld.Plugin2.Console.Configuration;
 using Microsoft.SemanticKernel.Planning.Handlebars;
+using HelloWorld.Planner1.Console.Utilities;
 
 internal class Program
 {
@@ -45,41 +46,29 @@ internal class Program
         builder.Plugins.AddFromType<DailyFactPlugin>();
                
         Kernel kernel = builder.Build();
-
-        var planner = new HandlebarsPlanner(new HandlebarsPlannerOptions() { AllowLoops = true });
-                        
+                
         // TODO: CHALLENGE 1: does the AI respond accurately to this prompt? How to fix?
-        var goal = $"Tell me an interesting fact from world about an event " +
+        var prompt = $"Tell me an interesting fact from world about an event " +
                     $"that took place on today's date. " +
                     $"Be sure to mention the date in history for context.";
 
-        HandlebarsPlan? plan = null;
-        var fileName = "SavedPlan.hbs";
-        if (File.Exists(fileName))
+        var planner = new HandlebarsPlanner(new HandlebarsPlannerOptions() { AllowLoops = true });
+
+        HandlebarsPlan plan;
+        if (SavedPlan.Exists)
         {
-            // Load the saved plan
-            var savedPlan = File.ReadAllText(fileName);
-            
-            // Populate intance
-            plan = new HandlebarsPlan(savedPlan);
+            plan = SavedPlan.LoadFromFile();
         }
         else
         {
-            // Call LLM to create the plan 
-            plan = await planner.CreatePlanAsync(kernel, goal);
-
-            // Save for next run
-            File.WriteAllText(fileName, plan.ToString());
-
-            WriteLine($"\nPLAN: \n\n{plan}");
+            plan = await planner.CreateAndSavePlanAsync(kernel, prompt);
         }
+       
+        WriteLine($"\nPLAN: \n\n{plan}");
 
-        if (plan != null)
-        {
-            var result = await plan.InvokeAsync(kernel);
+        var result = await plan.InvokeAsync(kernel);
 
-            WriteLine($"\nRESPONSE: \n\n{result}");
-        }
+        WriteLine($"\nRESPONSE: \n\n{result}");
     }
 
     static void WriteLine(string message)
---
+++ src/HelloWorld.Planner1.Console/Utilities/SavedPlan.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.Extensions.Options;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Planning.Handlebars;
+
+namespace HelloWorld.Planner1.Console.Utilities;
+internal static class SavedPlan
+{
+    public static string FileName = "SavedPlan.hbs";
+
+    public static bool Exists
+    {
+        get
+        {
+            return File.Exists(FileName);
+        }
+    }
+
+    public static HandlebarsPlan LoadFromFile()
+    {
+        // Load the saved plan
+        var savedPlan = File.ReadAllText(FileName);
+
+        // Populate intance
+        return new HandlebarsPlan(savedPlan);
+    }
+
+    public static void SaveToFile(HandlebarsPlan plan)
+    {
+        File.WriteAllText(FileName, plan.ToString());
+    }
+}
+
+public static class HandlebarsPlannerExtensions
+{
+    public async static Task<HandlebarsPlan> CreateAndSavePlanAsync(this HandlebarsPlanner planner, Kernel kernel, string goal, KernelArguments? arguments = null)
+    {
+        var plan = await planner.CreatePlanAsync(kernel, goal);
+
+        SavedPlan.SaveToFile(plan);
+
+        return plan;
+    }
+}
