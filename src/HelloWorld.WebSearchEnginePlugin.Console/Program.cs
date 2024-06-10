0e5f5dd01ec99f9348663ce9b6eaa2a19c4cd701
JasonHaley <jason@jasonhaley.com>
2024-06-10 08:07:59 -0400

    code for the search plugin blog post

---
+++ src/HelloWorld.WebSearchEnginePlugin.Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+using HelloWorld.Configuration;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Web;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MainAsync(args).Wait();
+    }
+
+    static async Task MainAsync(string[] args)
+    {
+        var config = Configuration.ConfigureAppSettings();
+
+        // Get Settings (all this is just so I don't have hard coded config settings here)
+        var openAiSettings = new OpenAIOptions();
+        config.GetSection(OpenAIOptions.OpenAI).Bind(openAiSettings);
+
+        var pluginSettings = new PluginOptions();
+        config.GetSection(PluginOptions.PluginConfig).Bind(pluginSettings);
+
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Information);
+
+            builder.AddConfiguration(config);
+            builder.AddConsole();
+        });
+
+        // Configure Semantic Kernel
+        var builder = Kernel.CreateBuilder();
+
+        builder.Services.AddSingleton(loggerFactory);
+        builder.AddChatCompletionService(openAiSettings);
+        //builder.AddChatCompletionService(openAiSettings, ApiLoggingLevel.ResponseAndRequest); // use this line to see the JSON between SK and OpenAI
+                
+        //builder.AddBingConnector(pluginSettings);
+        //builder.AddBingConnector(pluginSettings, ApiLoggingLevel.ResponseAndRequest); // use this line to see the JSON between SK and OpenAI
+        //builder.Plugins.AddFromType<WebSearchEnginePlugin>();
+
+        Kernel kernel = builder.Build();
+
+        var prompt = "Who are the organizers for the Noth Boston Azure meetup?";
+
+        WriteLine($"\nQUESTION: \n\n{prompt}");
+
+        OpenAIPromptExecutionSettings settings = new() 
+        { 
+            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions, 
+            Temperature = 0.7f,
+            MaxTokens = 250
+        };
+        
+        var funcresult = await kernel.InvokePromptAsync(prompt, new KernelArguments(settings));
+
+        WriteLine($"\nANSWER: \n\n{funcresult}");
+    }
+
+    static void WriteLine(string message)
+    {
+        Console.WriteLine("----------------------------------------------");
+
+        Console.WriteLine(message);
+
+        Console.WriteLine("----------------------------------------------");
+    }
+}
