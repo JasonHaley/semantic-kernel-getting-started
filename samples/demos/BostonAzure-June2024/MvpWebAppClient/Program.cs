f0db7366d7aa6a7ceb78042c2774200bc484f787
Jason Haley <jason@jasonhaley.com>
2024-06-24 09:41:10 -0400

    Added demo script

--- samples/demos/BostonAzure-June2024/MvpWebAppClient/Program.cs
+++ samples/demos/BostonAzure-June2024/MvpWebAppClient/Program.cs
@@ -3,10 +3,11 @@
 using HttpClient hc = new() { BaseAddress = new("http://localhost:5171") };
 while (true)
 {
-    Console.Write("Question: ");
+	Console.WriteLine("");
+	Console.Write("Question: ");
 
     await foreach (var msg in hc.GetFromJsonAsAsyncEnumerable<string>($"/copilot?question={Console.ReadLine()}"))
         Console.Write(msg);
-    
-    Console.WriteLine();
+
+	Console.WriteLine();
 }
